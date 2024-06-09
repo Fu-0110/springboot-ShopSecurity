9108f47b0c29c8cd3a00242384bfaab1523d7ff5
kuan08710 <kkuan1020@gmail.com>
2024-06-09 22:46:11 +0800

    初始化專案

---
+++ ShopSecurity/src/main/java/com/louis/shopsecurity/ShopSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.louis.shopsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopSecurityApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(ShopSecurityApplication.class , args);
+    }
+
+}
